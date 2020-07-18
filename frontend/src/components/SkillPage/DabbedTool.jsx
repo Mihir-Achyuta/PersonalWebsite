0db3f1d1266d3e0a2c2f34bb9db87f2b31e3fc19
Mihir <mihirachyuta@gmail.com>
2020-07-18 14:35:04 -0700

    Deleted dabbed section and put in all icons

--- frontend/src/components/SkillPage/DabbedTool.jsx
+++ frontend/src/components/SkillPage/DabbedTool.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Arrow from "./down.svg";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faJava } from '@fortawesome/free-brands-svg-icons'
+import { faPython } from '@fortawesome/free-brands-svg-icons'
+
 function DabbedTool(){
   return(
     <div>
       <div class = "DabbedThings">
         <h2>Things I've Dabbed In</h2>
-        <p>Java</p>
-        <p>Python</p>
+        <FontAwesomeIcon icon={faJava} class = "fa" /><br/>
+        <FontAwesomeIcon icon={faPython} class = "fa" /><br/>
       </div>
       <img src = {Arrow}></img>
     </div>
--- frontend/src/components/SkillPage/Skillz.jsx
+++ frontend/src/components/SkillPage/Skillz.jsx
@@ -13,7 +13,6 @@ function Skillz(){
       <FrontendTool/>
       <BackendTool/>
       <ToolTool/>
-      <DabbedTool/>
     </div>
   );
 }
