334e10ae2dc2ef2fc64687ffbfc31a827233a3f0
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-08-15 22:48:31 -0700

    Started styling navbar

--- src/components/AbsoluteIcons.jsx
+++ src/components/AbsoluteIcons.jsx
@@ -3,14 +3,14 @@ import React from "react";
 class AbsoluteIcons extends React.Component {
   render() {
     return (
-      <section>
+      <section className="Absolute-Icons">
         <img
-          className="Arrow-Up"
+          className="Absolute-Icons-Arrow-Up"
           src="https://img.icons8.com/android/24/000000/thick-arrow-pointing-up.png"
           alt="Up Arrow"
         />
         <img
-          className="Hamburger"
+          className="Absolute-Icons-Hamburger"
           src="https://img.icons8.com/android/24/000000/menu.png"
           alt="Hamburger Menu"
         />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return (
-      <header className="Header">
+      <div className="Header">
         <div className="Header-Logo">
           <h1 className="Header-Logo-Left">Mihir</h1>
         </div>
@@ -17,7 +17,7 @@ class Header extends React.Component {
             <li>Resume</li>
           </ul>
         </nav>
-      </header>
+      </div>
     );
   }
 }
