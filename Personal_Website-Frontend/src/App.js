a23c6fe844939a0a202af42de182efcfe20e481d
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-10-04 20:35:51 -0700

    Did some basic styles for the contact form

--- Personal_Website-Frontend/src/App.js
+++ Personal_Website-Frontend/src/App.js
@@ -9,56 +9,61 @@ import Footer from "./components/Footer";
 import AbsoluteIcons from "./components/AbsoluteIcons";
 import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="Header-Homepage">
-        <Header />
-        <Homepage />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="Header-Homepage">
+          <Header />
+          <Homepage />
+        </div>
+        <About />
+        <Skills />
+        <Projects
+          projectName="Project Name 1"
+          description="Lorem ipsum dolor sit amet, consectetur"
+          stack="Lorem ipsum dolor sit amet, consectetur"
+          imgUrl="https://picsum.photos/300/200"
+          backColor="#2f4858"
+          textColor="#7ae2dd"
+          topBorder="#14bdad"
+        />
+        <Projects
+          projectName="Project Name 2"
+          description="Lorem ipsum dolor sit amet, consectetur"
+          stack="Lorem ipsum dolor sit amet, consectetur"
+          imgUrl="https://picsum.photos/300/200"
+          backColor="#7ae2dd"
+          textColor="#2f4858"
+          topBorder="#2f4858"
+        />
+        <Projects
+          projectName="Project Name 3"
+          description="Lorem ipsum dolor sit amet, consectetur"
+          stack="Lorem ipsum dolor sit amet, consectetur"
+          imgUrl="https://picsum.photos/300/200"
+          backColor="#2f4858"
+          textColor="#14bdad"
+          topBorder="#7ae2dd"
+        />
+        <Projects
+          projectName="Project Name 4"
+          description="Lorem ipsum dolor sit amet, consectetur"
+          stack="Lorem ipsum dolor sit amet, consectetur"
+          imgUrl="https://picsum.photos/300/200"
+          backColor="#14bdad"
+          textColor="#2f4858"
+          topBorder="#2f4858"
+        />
+        <Contact />
+        <Footer />
+        <AbsoluteIcons />
       </div>
-      <About />
-      <Skills />
-      <Projects
-        projectName="Project Name 1"
-        description="Lorem ipsum dolor sit amet, consectetur"
-        stack="Lorem ipsum dolor sit amet, consectetur"
-        imgUrl="https://picsum.photos/300/200"
-        backColor="#2f4858"
-        textColor="#7ae2dd"
-        topBorder="#14bdad"
-      />
-      <Projects
-        projectName="Project Name 2"
-        description="Lorem ipsum dolor sit amet, consectetur"
-        stack="Lorem ipsum dolor sit amet, consectetur"
-        imgUrl="https://picsum.photos/300/200"
-        backColor="#7ae2dd"
-        textColor="#2f4858"
-        topBorder="#2f4858"
-      />
-      <Projects
-        projectName="Project Name 3"
-        description="Lorem ipsum dolor sit amet, consectetur"
-        stack="Lorem ipsum dolor sit amet, consectetur"
-        imgUrl="https://picsum.photos/300/200"
-        backColor="#2f4858"
-        textColor="#14bdad"
-        topBorder="#7ae2dd"
-      />
-      <Projects
-        projectName="Project Name 4"
-        description="Lorem ipsum dolor sit amet, consectetur"
-        stack="Lorem ipsum dolor sit amet, consectetur"
-        imgUrl="https://picsum.photos/300/200"
-        backColor="#14bdad"
-        textColor="#2f4858"
-        topBorder="#2f4858"
-      />
-      <Contact />
-      <Footer />
-      <AbsoluteIcons />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- Personal_Website-Frontend/src/components/Contact.jsx
+++ Personal_Website-Frontend/src/components/Contact.jsx
@@ -4,30 +4,33 @@ class Contact extends React.Component {
   render() {
     return (
       <section className="Contact">
-        <h1>Contact Me!</h1>
-        <form className="Contact-Form">
-          <div className="Contact-Name-Div">
-            <input
-              type="text"
-              placeholder="Enter Name"
-              className="Contact-Name-Text"
-            ></input>
-          </div>
-          <div className="Contact-Email-Div">
-            <input
-              className="Contact-Email-Text"
-              type="text"
-              placeholder="Enter Email"
-            ></input>
-          </div>
-          <div className="Contact-Message-Div">
-            <textarea
-              className="Contact-Message-Text"
-              placeholder="Enter Message"
-            ></textarea>
-          </div>
-          <input type="submit" value="Submit"></input>
-        </form>
+        <div className="Contact-Div">
+          <h1 className="Contact-Header">Contact Me!</h1>
+          <form className="Contact-Form">
+            <div className="Contact-Name-Div">
+              <input
+                type="text"
+                placeholder="Enter Name"
+                className="Contact-Name-Text Input-Field"
+              ></input>
+            </div>
+            <div className="Contact-Email-Div">
+              <input
+                className="Contact-Email-Text Input-Field"
+                type="text"
+                placeholder="Enter Email"
+              ></input>
+            </div>
+            <div className="Contact-Message-Div">
+              <input
+                className="Contact-Message-Text Input-Field"
+                placeholder="Enter Message"
+                type="text"
+              ></input>
+            </div>
+            <button className="Contact-Button">SUBMIT</button>
+          </form>
+        </div>
       </section>
     );
   }
