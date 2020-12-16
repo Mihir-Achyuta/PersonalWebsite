39d3433e8e7b69ed751f795c4b0c7bcf82000798
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-12-16 01:07:34 -0800

    Seperated regular website into Page component for App.js

--- Personal_Website-Frontend/src/App.js
+++ Personal_Website-Frontend/src/App.js
@@ -2,34 +2,15 @@ import React from "react";
 
 import "./styles/App.css";
 
+import Page from "./components/Page";
 import LoadingAnimation from "./components/page-components/LoadingAnimation";
-import Header from "./components/page-components/Header";
-import Homepage from "./components/page-components/Homepage";
-import About from "./components/page-components/About";
-import Skills from "./components/page-components/Skills";
-import Project from "./components/page-components/Project";
-import Contact from "./components/page-components/Contact";
-import Footer from "./components/page-components/Footer";
-import AbsoluteIcons from "./components/page-components/AbsoluteIcons";
-
-import TopBurgerMenu from "./components/mini-components/burger-components/TopBurgerMenu";
-import BottomBurgerMenu from "./components/mini-components/burger-components/BottomBurgerMenu";
-import Cursor from "./components/mini-components/link-components/Cursor";
-
-import { Element } from "react-scroll";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       contentLoaded: false,
-      bottomMenuShown: false,
-      burgerMenuShown: false,
     };
-    this.showBottomMenu = this.showBottomMenu.bind(this);
-    this.dissapearBottomMenu = this.dissapearBottomMenu.bind(this);
-    this.showBurgerMenu = this.showBurgerMenu.bind(this);
-    this.dissapearBurgerMenu = this.dissapearBurgerMenu.bind(this);
   }
 
   componentDidMount() {
@@ -43,110 +24,10 @@ class App extends React.Component {
     }, 2500);
   }
 
-  scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  }
-
-  showBottomMenu() {
-    this.setState({ bottomMenuShown: true });
-  }
-
-  dissapearBottomMenu() {
-    this.setState({ bottomMenuShown: false });
-  }
-
-  showBurgerMenu() {
-    this.setState({ burgerMenuShown: true });
-  }
-
-  dissapearBurgerMenu() {
-    this.setState({ burgerMenuShown: false });
-  }
-
   render() {
     return (
       <div className="App">
-        {this.state.contentLoaded ? (
-          <div className="App-Div">
-            {this.state.bottomMenuShown ? (
-              <div className="Menu Menu-Load">
-                <BottomBurgerMenu
-                  scrollTop={this.scrollToTop}
-                  dissapearBottomMenu={this.dissapearBottomMenu}
-                />
-              </div>
-            ) : (
-              <div className="Menu"></div>
-            )}
-            {this.state.burgerMenuShown ? (
-              <div className="Burger-Menu Burger-Menu-Load">
-                <TopBurgerMenu
-                  scrollTop={this.scrollToTop}
-                  dissapearBurgerMenu={this.dissapearBurgerMenu}
-                />
-              </div>
-            ) : (
-              <div className="Burger-Menu"></div>
-            )}
-            <div className="Header-Homepage">
-              <Cursor />
-              <Header
-                scrollTop={this.scrollToTop}
-                showBurgerMenu={this.showBurgerMenu}
-              />
-              <Homepage />
-            </div>
-            <Element name="about">
-              {" "}
-              <About />
-            </Element>
-            <Element name="skills">
-              <Skills />
-            </Element>
-            <Element name="projects">
-              <Project
-                projectName="Project Name 1"
-                description="Lorem ipsum dolor sit amet, consectetur"
-                stack="Lorem ipsum dolor sit amet, consectetur"
-                imgUrl="https://picsum.photos/300/200"
-                fadeLeft
-              />
-              <Project
-                projectName="Project Name 2"
-                description="Lorem ipsum dolor sit amet, consectetur"
-                stack="Lorem ipsum dolor sit amet, consectetur"
-                imgUrl="https://picsum.photos/300/200"
-              />
-              <Project
-                projectName="Project Name 3"
-                description="Lorem ipsum dolor sit amet, consectetur"
-                stack="Lorem ipsum dolor sit amet, consectetur"
-                imgUrl="https://picsum.photos/300/200"
-                fadeLeft
-              />
-              <Project
-                projectName="Project Name 4"
-                description="Lorem ipsum dolor sit amet, consectetur"
-                stack="Lorem ipsum dolor sit amet, consectetur"
-                imgUrl="https://picsum.photos/300/200"
-              />
-            </Element>
-            <Element name="contact">
-              <Contact />
-            </Element>
-            <Footer />
-            <AbsoluteIcons
-              showMenu={this.showBottomMenu}
-              scrollTop={this.scrollToTop}
-            />
-          </div>
-        ) : (
-          <LoadingAnimation />
-        )}
+        {this.state.contentLoaded ? <Page /> : <LoadingAnimation />}
       </div>
     );
   }
---
+++ Personal_Website-Frontend/src/components/Page.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+
+import Header from "./page-components/Header";
+import Homepage from "./page-components/Homepage";
+import About from "./page-components/About";
+import Skills from "./page-components/Skills";
+import Project from "./page-components/Project";
+import Contact from "./page-components/Contact";
+import Footer from "./page-components/Footer";
+import AbsoluteIcons from "./page-components/AbsoluteIcons";
+
+import TopBurgerMenu from "./mini-components/burger-components/TopBurgerMenu";
+import BottomBurgerMenu from "./mini-components/burger-components/BottomBurgerMenu";
+import Cursor from "./mini-components/link-components/Cursor";
+
+import { Element } from "react-scroll";
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bottomMenuShown: false,
+      burgerMenuShown: false,
+    };
+    this.showBottomMenu = this.showBottomMenu.bind(this);
+    this.dissapearBottomMenu = this.dissapearBottomMenu.bind(this);
+    this.showBurgerMenu = this.showBurgerMenu.bind(this);
+    this.dissapearBurgerMenu = this.dissapearBurgerMenu.bind(this);
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+
+  showBottomMenu() {
+    this.setState({ bottomMenuShown: true });
+  }
+
+  dissapearBottomMenu() {
+    this.setState({ bottomMenuShown: false });
+  }
+
+  showBurgerMenu() {
+    this.setState({ burgerMenuShown: true });
+  }
+
+  dissapearBurgerMenu() {
+    this.setState({ burgerMenuShown: false });
+  }
+  render() {
+    return (
+      <div className="App-Div">
+        {this.state.bottomMenuShown ? (
+          <div className="Menu Menu-Load">
+            <BottomBurgerMenu
+              scrollTop={this.scrollToTop}
+              dissapearBottomMenu={this.dissapearBottomMenu}
+            />
+          </div>
+        ) : (
+          <div className="Menu"></div>
+        )}
+        {this.state.burgerMenuShown ? (
+          <div className="Burger-Menu Burger-Menu-Load">
+            <TopBurgerMenu
+              scrollTop={this.scrollToTop}
+              dissapearBurgerMenu={this.dissapearBurgerMenu}
+            />
+          </div>
+        ) : (
+          <div className="Burger-Menu"></div>
+        )}
+        <div className="Header-Homepage">
+          <Cursor />
+          <Header
+            scrollTop={this.scrollToTop}
+            showBurgerMenu={this.showBurgerMenu}
+          />
+          <Homepage />
+        </div>
+        <Element name="about">
+          {" "}
+          <About />
+        </Element>
+        <Element name="skills">
+          <Skills />
+        </Element>
+        <Element name="projects">
+          <Project
+            projectName="Project Name 1"
+            description="Lorem ipsum dolor sit amet, consectetur"
+            stack="Lorem ipsum dolor sit amet, consectetur"
+            imgUrl="https://picsum.photos/300/200"
+            fadeLeft
+          />
+          <Project
+            projectName="Project Name 2"
+            description="Lorem ipsum dolor sit amet, consectetur"
+            stack="Lorem ipsum dolor sit amet, consectetur"
+            imgUrl="https://picsum.photos/300/200"
+          />
+          <Project
+            projectName="Project Name 3"
+            description="Lorem ipsum dolor sit amet, consectetur"
+            stack="Lorem ipsum dolor sit amet, consectetur"
+            imgUrl="https://picsum.photos/300/200"
+            fadeLeft
+          />
+          <Project
+            projectName="Project Name 4"
+            description="Lorem ipsum dolor sit amet, consectetur"
+            stack="Lorem ipsum dolor sit amet, consectetur"
+            imgUrl="https://picsum.photos/300/200"
+          />
+        </Element>
+        <Element name="contact">
+          <Contact />
+        </Element>
+        <Footer />
+        <AbsoluteIcons
+          showMenu={this.showBottomMenu}
+          scrollTop={this.scrollToTop}
+        />
+      </div>
+    );
+  }
+}
+
+export default Page;
