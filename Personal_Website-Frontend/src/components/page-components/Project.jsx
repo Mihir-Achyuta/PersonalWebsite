d540f872d05fd2dd623d50a1c32b4825b7025e4e
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-01-02 17:34:54 -0800

    Changed projects and projects-one classes to a simple project class

--- Personal_Website-Frontend/src/components/page-components/Project.jsx
+++ Personal_Website-Frontend/src/components/page-components/Project.jsx
@@ -7,36 +7,34 @@ import Fade from "react-reveal/Fade";
 class Project extends React.Component {
   render() {
     return (
-      <section className="Projects">
+      <section className="Project">
         <Fade left={this.props.fadeLeft} right={!this.props.fadeLeft}>
-          <h1 className="Projects-One-Name">{this.props.projectName}</h1>
-          <div className="Projects-One-Div">
-            <div className="Projects-One-Text">
-              <div className="Projects-One-Description-Div">
-                <h2 className="Projects-One-Description-Header">
-                  Description:
-                </h2>
-                <p className="Projects-One-Description-Text">
+          <h1 className="Project-Name">{this.props.projectName}</h1>
+          <div className="Project-Div">
+            <div className="Project-Text">
+              <div className="Project-Description-Div">
+                <h2 className="Project-Description-Header">Description:</h2>
+                <p className="Project-Description-Text">
                   {this.props.description}
                 </p>
               </div>
-              <div className="Projects-One-Stack-Div">
-                <h2 className="Projects-One-Stack-Header">Tech Stack:</h2>
-                <p className="Projects-One-Stack-Text">{this.props.stack}</p>
+              <div className="Project-Stack-Div">
+                <h2 className="Project-Stack-Header">Tech Stack:</h2>
+                <p className="Project-Stack-Text">{this.props.stack}</p>
               </div>
             </div>
-            <div className="Projects-One-Images">
-              <div className="Projects-One-Image">
+            <div className="Project-Images">
+              <div className="Project-Image">
                 <img src={this.props.imgUrl} alt="Project"></img>
               </div>
-              <div className="Projects-One-Icons">
+              <div className="Project-Icons">
                 <a
                   href="https://github.com/Mihir-Achyuta"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <FontAwesomeIcon
-                    className="Projects-One-Icon"
+                    className="Project-Icon"
                     icon={faGithub}
                     size="2x"
                   />
@@ -48,7 +46,7 @@ class Project extends React.Component {
                 >
                   <FontAwesomeIcon
                     color={"#84a7bd"}
-                    className="Projects-One-Icon"
+                    className="Project-Icon"
                     icon={faEye}
                     size="2x"
                   />
