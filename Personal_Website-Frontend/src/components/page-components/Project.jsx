f26dfaf9ab0b58de3fa539946051141f0c370707
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-01-02 17:10:55 -0800

    Removed the original ternary for fade left/right animations and instead modified the attribute to conditionally render the animation

--- Personal_Website-Frontend/src/components/page-components/Project.jsx
+++ Personal_Website-Frontend/src/components/page-components/Project.jsx
@@ -8,106 +8,55 @@ class Project extends React.Component {
   render() {
     return (
       <section className="Projects">
-        {this.props.fadeLeft ? (
-          <Fade left>
-            <h1 className="Projects-One-Name">{this.props.projectName}</h1>
-            <div className="Projects-One-Div">
-              <div className="Projects-One-Text">
-                <div className="Projects-One-Description-Div">
-                  <h2 className="Projects-One-Description-Header">
-                    Description:
-                  </h2>
-                  <p className="Projects-One-Description-Text">
-                    {this.props.description}
-                  </p>
-                </div>
-                <div className="Projects-One-Stack-Div">
-                  <h2 className="Projects-One-Stack-Header">Tech Stack:</h2>
-                  <p className="Projects-One-Stack-Text">{this.props.stack}</p>
-                </div>
+        <Fade left={this.props.fadeLeft} right={!this.props.fadeLeft}>
+          <h1 className="Projects-One-Name">{this.props.projectName}</h1>
+          <div className="Projects-One-Div">
+            <div className="Projects-One-Text">
+              <div className="Projects-One-Description-Div">
+                <h2 className="Projects-One-Description-Header">
+                  Description:
+                </h2>
+                <p className="Projects-One-Description-Text">
+                  {this.props.description}
+                </p>
               </div>
-              <div className="Projects-One-Images">
-                <div className="Projects-One-Image">
-                  <img src={this.props.imgUrl} alt="Project"></img>
-                </div>
-                <div className="Projects-One-Icons">
-                  <a
-                    href="https://github.com/Mihir-Achyuta"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FontAwesomeIcon
-                      className="Projects-One-Icon"
-                      icon={faGithub}
-                      size="2x"
-                    />
-                  </a>
-                  <a
-                    href={this.props.projectUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FontAwesomeIcon
-                      color={"#84a7bd"}
-                      className="Projects-One-Icon"
-                      icon={faEye}
-                      size="2x"
-                    />
-                  </a>
-                </div>
+              <div className="Projects-One-Stack-Div">
+                <h2 className="Projects-One-Stack-Header">Tech Stack:</h2>
+                <p className="Projects-One-Stack-Text">{this.props.stack}</p>
               </div>
             </div>
-          </Fade>
-        ) : (
-          <Fade right>
-            <h1 className="Projects-One-Name">{this.props.projectName}</h1>
-            <div className="Projects-One-Div">
-              <div className="Projects-One-Text">
-                <div className="Projects-One-Description-Div">
-                  <h2 className="Projects-One-Description-Header">
-                    Description:
-                  </h2>
-                  <p className="Projects-One-Description-Text">
-                    {this.props.description}
-                  </p>
-                </div>
-                <div className="Projects-One-Stack-Div">
-                  <h2 className="Projects-One-Stack-Header">Tech Stack:</h2>
-                  <p className="Projects-One-Stack-Text">{this.props.stack}</p>
-                </div>
+            <div className="Projects-One-Images">
+              <div className="Projects-One-Image">
+                <img src={this.props.imgUrl} alt="Project"></img>
               </div>
-              <div className="Projects-One-Images">
-                <div className="Projects-One-Image">
-                  <img src={this.props.imgUrl} alt="Project"></img>
-                </div>
-                <div className="Projects-One-Icons">
-                  <a
-                    href="https://github.com/Mihir-Achyuta"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FontAwesomeIcon
-                      className="Projects-One-Icon"
-                      icon={faGithub}
-                      size="2x"
-                    />
-                  </a>
-                  <a
-                    href={this.props.projectUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FontAwesomeIcon
-                      className="Projects-One-Icon"
-                      icon={faEye}
-                      size="2x"
-                    />
-                  </a>
-                </div>
+              <div className="Projects-One-Icons">
+                <a
+                  href="https://github.com/Mihir-Achyuta"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    className="Projects-One-Icon"
+                    icon={faGithub}
+                    size="2x"
+                  />
+                </a>
+                <a
+                  href={this.props.projectUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    color={"#84a7bd"}
+                    className="Projects-One-Icon"
+                    icon={faEye}
+                    size="2x"
+                  />
+                </a>
               </div>
             </div>
-          </Fade>
-        )}
+          </div>
+        </Fade>
       </section>
     );
   }
