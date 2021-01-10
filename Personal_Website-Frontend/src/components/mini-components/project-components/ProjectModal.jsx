a9c4e2717ecc87cfcc34724bbe90e7c9437ee9f0
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-01-09 21:59:41 -0800

    Removed the portfolio project info and will seperate into own modal

---
+++ Personal_Website-Frontend/src/components/mini-components/project-components/ProjectModal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ProjectModal extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Project Name</h1>
+      </div>
+    );
+  }
+}
+
+export default ProjectModal;
