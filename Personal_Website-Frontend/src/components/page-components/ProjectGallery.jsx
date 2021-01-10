805d3f160f5a07f5b90b5a1b74b29e9ae7ba8e81
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-01-09 23:00:15 -0800

    Added rough project image structure and purged original project description styles

--- Personal_Website-Frontend/src/components/page-components/ProjectGallery.jsx
+++ Personal_Website-Frontend/src/components/page-components/ProjectGallery.jsx
@@ -8,42 +8,44 @@ class ProjectGallery extends React.Component {
     return (
       <div className="Project-Gallery">
         <h1 className="Project-Gallery-Header">Portfolio</h1>
-        <Project
-          projectName="Portfolio Website"
-          description="My website where you can view my web and mobile application projects utilizing various technologies."
-          stack={["React.JS", "Node.JS"]}
-          projectPicture={ProfilePic}
-          projectUrl="https://mihir-achyuta.netlify.app/"
-          githubUrl="https://github.com/Mihir-Achyuta"
-          fadeLeft
-        />
-        <Project
-          projectName="Portfolio Website"
-          description="My website where you can view my web and mobile application projects utilizing various technologies."
-          stack={["HTML", "CSS", "JavaScript", "React", "Node", "Express"]}
-          projectPicture={ProfilePic}
-          projectUrl="https://mihir-achyuta.netlify.app/"
-          githubUrl="https://github.com/Mihir-Achyuta"
-          fadeLeft
-        />
-        <Project
-          projectName="Portfolio Website"
-          description="My website where you can view my web and mobile application projects utilizing various technologies."
-          stack={["React.JS", "Node.JS"]}
-          projectPicture={ProfilePic}
-          projectUrl="https://mihir-achyuta.netlify.app/"
-          githubUrl="https://github.com/Mihir-Achyuta"
-          fadeLeft
-        />
-        <Project
-          projectName="Portfolio Website"
-          description="My website where you can view my web and mobile application projects utilizing various technologies."
-          stack={["HTML", "CSS", "JavaScript", "React", "Node", "Express"]}
-          projectPicture={ProfilePic}
-          projectUrl="https://mihir-achyuta.netlify.app/"
-          githubUrl="https://github.com/Mihir-Achyuta"
-          fadeLeft
-        />
+        <div className="Project-Gallery-Projects">
+          <Project
+            projectName="Portfolio Website"
+            description="My website where you can view my web and mobile application projects utilizing various technologies."
+            stack={["React.JS", "Node.JS"]}
+            projectPicture={ProfilePic}
+            projectUrl="https://mihir-achyuta.netlify.app/"
+            githubUrl="https://github.com/Mihir-Achyuta"
+            fadeLeft
+          />
+          <Project
+            projectName="Portfolio Website"
+            description="My website where you can view my web and mobile application projects utilizing various technologies."
+            stack={["HTML", "CSS", "JavaScript", "React", "Node", "Express"]}
+            projectPicture={ProfilePic}
+            projectUrl="https://mihir-achyuta.netlify.app/"
+            githubUrl="https://github.com/Mihir-Achyuta"
+            fadeLeft
+          />
+          <Project
+            projectName="Portfolio Website"
+            description="My website where you can view my web and mobile application projects utilizing various technologies."
+            stack={["React.JS", "Node.JS"]}
+            projectPicture={ProfilePic}
+            projectUrl="https://mihir-achyuta.netlify.app/"
+            githubUrl="https://github.com/Mihir-Achyuta"
+            fadeLeft
+          />
+          <Project
+            projectName="Portfolio Website"
+            description="My website where you can view my web and mobile application projects utilizing various technologies."
+            stack={["HTML", "CSS", "JavaScript", "React", "Node", "Express"]}
+            projectPicture={ProfilePic}
+            projectUrl="https://mihir-achyuta.netlify.app/"
+            githubUrl="https://github.com/Mihir-Achyuta"
+            fadeLeft
+          />
+        </div>
       </div>
     );
   }
