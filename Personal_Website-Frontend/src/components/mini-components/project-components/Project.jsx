bcd4da443bd88f908336a73fe72a395c09f2ed2c
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-01-18 01:09:54 -0800

    Made image dissapear and text appear on hover and added project information boilerplate content

--- Personal_Website-Frontend/src/components/mini-components/project-components/Project.jsx
+++ Personal_Website-Frontend/src/components/mini-components/project-components/Project.jsx
@@ -8,20 +8,51 @@ class Project extends React.Component {
   constructor(props) {
     super(props);
     this.state = { projectPreview: false };
+    this.showProjectPreview = this.showProjectPreview.bind(this);
+    this.removeProjectPreview = this.removeProjectPreview.bind(this);
+  }
+  showProjectPreview() {
+    this.setState({ projectPreview: true });
+  }
+  removeProjectPreview() {
+    this.setState({ projectPreview: false });
   }
   render() {
+    const technologyDescription = this.props.stack.map((technology, index) => (
+      <p key={index}>{technology}</p>
+    ));
     return (
       <Fade>
         <div className="Project">
-          <div className="Project-Image-Div">
+          <div
+            className={
+              this.state.projectPreview
+                ? "Project-Image-Div-Hovered"
+                : "Project-Image-Div"
+            }
+            onMouseEnter={this.showProjectPreview}
+            onMouseOut={this.removeProjectPreview}
+          >
             <img
               className="Project-Image"
               src={this.props.projectPicture}
               alt="Profile Pic"
             />
           </div>
-          <div className="Project-Information-Div">
-            <h1>{this.props.projectName}</h1>
+          <div
+            className={
+              this.state.projectPreview
+                ? "Project-Information-Div-Hovered"
+                : "Project-Information-Div"
+            }
+          >
+            <h1 className="Project-Information-Name">
+              {this.props.projectName}
+            </h1>
+            <div className={"Project-Information-Technologies"}>
+              {technologyDescription}
+            </div>
+            <button className="Project-Information-Button">More Details</button>
           </div>
         </div>
       </Fade>
