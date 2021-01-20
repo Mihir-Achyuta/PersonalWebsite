e5e83981b6caa23c85f42832c976c6968ce2d7fd
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-01-19 21:19:59 -0800

    Made projects responsive from 2x2 grid to 1x4 on mobile

---
+++ build/build/precache-manifest.1212e27e79d8dc0599e7f77459c40074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038d33a64f12061f86e55bb435528711",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4168303ec7b05ddad148",
+    "url": "/static/css/main.36b7c7e1.chunk.css"
+  },
+  {
+    "revision": "125c3f3432eedcb6e068",
+    "url": "/static/js/2.bcaa19e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcaa19e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4168303ec7b05ddad148",
+    "url": "/static/js/main.d26843ef.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
---
+++ build/precache-manifest.8b860d2d2906e1f76c8d8fb2581f62e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ad92d1e0cb2f275c5c62bf302cbc4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d82f2dcd29d7109858",
+    "url": "/static/css/main.7372f578.chunk.css"
+  },
+  {
+    "revision": "6d6d916e7cbe70d4cac2",
+    "url": "/static/js/2.e8265ebb.chunk.js"
+  },
+  {
+    "revision": "f75561adb7f4db3b610232e7231a7ed4",
+    "url": "/static/js/2.e8265ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d82f2dcd29d7109858",
+    "url": "/static/js/main.c9748f85.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "9830c779f7cd4d22fa43520c4915f47e",
+    "url": "/static/media/profile-image.9830c779.png"
+  },
+  {
+    "revision": "5f175be7d768cf4073a1b977c51392fb",
+    "url": "/static/media/project-picture.5f175be7.png"
+  },
+  {
+    "revision": "b2d0605788058bf835f0d53fc69e27d9",
+    "url": "/static/media/resume.b2d06057.pdf"
+  }
+]);
