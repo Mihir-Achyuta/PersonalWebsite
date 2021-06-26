e7ffae9cf5f4884d0ce799bdc6d2fc1130230ab4
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-06-25 17:13:12 -0700

    Deleted skill components

--- src/components/PageComponents/Skills.tsx
+++ src/components/PageComponents/Skills.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import SkillCard from "../MiniComponents/SkillComponents/SkillCard";
-
 import Fade from "react-reveal/Fade";
 
 class Skills extends React.Component {
@@ -11,49 +9,6 @@ class Skills extends React.Component {
         <Fade left>
           <h1 className="Skills-Header">What I Work With :</h1>
         </Fade>
-        <div className="Front-Back-Tool">
-          <Fade delay={500}>
-            <SkillCard
-              path="Frontend"
-              technologies={[
-                "HTML",
-                "CSS",
-                "Sass",
-                "Bootstrap",
-                "Javascript",
-                "jQuery",
-                "React.js",
-              ]}
-            />
-          </Fade>
-          <Fade delay={1000}>
-            <SkillCard
-              path="Backend"
-              technologies={[
-                "Node.js",
-                "Express.js",
-                "EJS",
-                "Postgres-SQL",
-                "MongoDB",
-                "Mongoose",
-                "Passport.js + OAuth",
-                "REST",
-              ]}
-            />
-          </Fade>
-          <Fade delay={1500}>
-            <SkillCard
-              path="Tools"
-              technologies={[
-                "Git/Git-Bash",
-                "Github",
-                "Hyper",
-                "Atom",
-                "MongoDB Atlas",
-              ]}
-            />
-          </Fade>
-        </div>
       </section>
     );
   }
